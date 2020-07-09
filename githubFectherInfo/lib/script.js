126c1b11ded0f0a427c3058b0b5b0586787b47d5
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-07-08 22:38:39 -0300

    Directives y Services

--- githubFectherInfo/lib/script.js
+++ githubFectherInfo/lib/script.js
@@ -3,21 +3,45 @@ import angular from 'angular';
 
   var app = angular.module("githubViewer", []);
   
-  var MainController = function($scope, $http){
+  var MainController = function($scope, $http, $interval){
     var onUserComplete = function(response){
       $scope.user = response.data;
+      $http.get($scope.user.repos_url)
+           .then(onRepos, onError)
+    };
+
+    var onRepos = function(response){
+      $scope.repos = response.data;
     };
 
     var onError = function(reason){
       $scope.error = "Could not fetch user"
     };
 
-    $http.get("https://api.github.com/users/cziemba1")
-      .then(onUserComplete, onError);
-    
-    $scope.message = "Hola Claudia";
+    var decrementCount = function(){
+      $scope.countdown -= 1;
+      if($scope.countdown < 1){
+        $scope.search($scope.username);
+      }
+    }
+
+    var startCountDown = function(){
+      $interval(decrementCount, 1000, $scope.countdown); 
+    }
+
+    $scope.search = function(username){
+      $http.get("https://api.github.com/users/" + username)
+           .then(onUserComplete, onError);
+    }
+    $scope.username= "angular";
+    $scope.message = "Github Searcher";
+    $scope.repoSortOrder = "name";
+    $scope.countdown = 5;
+    startCountDown();
   }
-  app.controller("MainController",["$scope", "$http", MainController]);
+
+  
+  app.controller("MainController", MainController);
 
 
 }());
